0a15c27151b04b549fdf3866f320b3a4299cde70
LeeJonghyeon <ditlswlwhs3@gmail.com>
2024-05-16 17:51:48 +0900

    필수 라이브러리 추가 및 프로젝트 설정 업데이트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,38 @@
 /* eslint-disable */
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
+import "./App.css";
+import { useState } from "react";
+// react-icons
+import { FaAngellist } from "react-icons/fa";
+// bootstrap lib
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Nav } from "react-bootstrap";
+// react-router-dom lib
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// custom
 
 function App() {
 
-  let [postName, b]=useState(['남자코트 추천'])
-
   return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>ReactBlog</h4>
+    <BrowserRouter>
+      <div className="App">
+        <FaAngellist></FaAngellist>
+        <h1 className="text-3xl font-bold underline">hello world</h1>
+        <Nav variant="pills" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+            <Button as="input" type="button" value="Input" />{" "}
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
-      <div className="list">
-        <h4>{ postName }</h4>
-        <p>2월 67일</p>
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
