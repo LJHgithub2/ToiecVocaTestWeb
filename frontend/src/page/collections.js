d039f9cc1211d077faacbd4d7621e3a3abb682b0
LeeJonghyeon <ditlswlwhs3@gmail.com>
2024-05-23 00:19:36 +0900

    일정 페이지 추가

--- frontend/src/page/collections.js
+++ frontend/src/page/collections.js
@@ -1,56 +1,70 @@
+import { useNavigate  } from "react-router-dom";
 const callouts = [
-    {
-      name: 'Desk and Office',
-      description: 'Work from home accessories',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-      href: '#',
-    },
-    {
-      name: 'Self-Improvement',
-      description: 'Journals and note-taking',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-      href: '#',
-    },
-    {
-      name: 'Travel',
-      description: 'Daily commute essentials',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-      href: '#',
-    },
-  ]
-  
-  export default function Collections() {
-    return (
-      <div className="bg-gray-100">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
-  
-            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-              {callouts.map((callout) => (
-                <div key={callout.name} className="group relative">
-                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                    <img
-                      src={callout.imageSrc}
-                      alt={callout.imageAlt}
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </div>
-                  <h3 className="mt-6 text-sm text-gray-500">
-                    <a href={callout.href}>
-                      <span className="absolute inset-0" />
-                      {callout.name}
-                    </a>
-                  </h3>
-                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+  {
+    name: "노랭이 단어장",
+    description: "헤커스 단어장입니다.",
+    imageSrc: "/image/collections/1.jpg",
+    imageAlt: "단어장 이미지",
+    href: "/nav/collections/list",
+  },
+  {
+    name: "Self-Improvement",
+    description: "Journals and note-taking",
+    imageSrc: "/image/collections/2.jpg",
+    imageAlt:
+      "Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.",
+    href: "/nav/collections/list",
+  },
+  {
+    name: "Travel",
+    description: "Daily commute essentials",
+    imageSrc: "/image/collections/3.jpg",
+    imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
+    href: "/nav/collections/list",
+  },
+];
+
+export default function Collections() {
+  const navigate = useNavigate();
+  return (
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-7xl px-4 xs:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-5 lg:max-w-none">
+          <h2 className="text-2xl font-bold text-gray-900">단어장 목록</h2>
+
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {callouts.map((callout) => (
+              <div key={callout.name} className="">
+                <div className="h-80 w-full hover:opacity-75 overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 sm:h-64">
+                  <img
+                    src={callout.imageSrc}
+                    alt={callout.imageAlt}
+                    className="h-full w-full object-cover object-center"
+                  />
                 </div>
-              ))}
-            </div>
+                <div className="flex justify-between items-center gap-x-2">
+                  <div>
+                    <h2 className="mt-6 text-sm text-gray-500">
+                      <span className="" />
+                      {callout.name}
+                    </h2>
+                    <p className="text-base font-semibold text-gray-900">
+                      {callout.description}
+                    </p>
+                  </div>
+                  <button
+                    type="button"
+                    className="text-pretty inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 w-auto sm:text-sm"
+                    onClick={()=>{navigate(callout.href);}}
+                  >
+                    단어장 보기
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
