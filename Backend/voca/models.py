ebb07980abaa3c6c7550de7e31028dd30709ac5a
LeeJonghyeon <ditlswlwhs3@gmail.com>
2024-05-25 14:15:37 +0900

    회원 및 프로필 DB 모델 설정

--- Backend/voca/models.py
+++ Backend/voca/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    groups = models.ManyToManyField(
+        Group,
+        related_name='voca_user_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_query_name='voca_user'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='voca_user_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='voca_user'
+    )
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
