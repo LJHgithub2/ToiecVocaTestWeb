c9b61ae54e28c54a8572cd7c2048b6557b3b3ed4
LeeJonghyeon <ditlswlwhs3@gmail.com>
2024-08-17 14:31:16 +0900

    [프론트엔드] 단어 추가 api 연동 및 파일 경로 변경

--- frontend/src/services/wordService.js
+++ frontend/src/services/wordService.js
@@ -2,13 +2,29 @@ import axios from '../config/axiosConfig';
 
 export const getPublicWords = async (vocab_id) => {
     try {
-        console.log(vocab_id);
-        const response = await axios.get(`/api/vocabularies/${vocab_id}/`);
-        console.log(response.data.words);
+        const response = await axios.get(
+            `/api/vocabularies/public/${vocab_id}/`
+        );
+        // console.log(response.data.words);
         if (!response.data.isAuthenticated) return null;
         return response.data.words;
     } catch (error) {
         console.error('Error fetching vocabularies data:', error);
         throw error;
+    }
+};
+
+export const addWord = async (vocab_id, inputValues) => {
+    try {
+        const response = await axios.post(
+            `/api/vocabularies/public/${vocab_id}/words/`,
+            inputValues
+        );
+        console.log(response);
+        return { data: response.data, status: response.status };
+    } catch (error) {
+        var response = error.response;
+        console.error('There was an error!', error);
+        return { data: response.data, status: response.status };
     }
 };
