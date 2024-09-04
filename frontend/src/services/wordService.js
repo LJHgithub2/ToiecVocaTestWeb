9cd3912d826319831fc064870cc8aa2aeeb5aac6
LeeJonghyeon <ditlswlwhs3@gmail.com>
2024-09-04 23:58:53 +0900

    [프론트엔드] getword url 변경(page 매개변수 추가)

--- frontend/src/services/wordService.js
+++ frontend/src/services/wordService.js
@@ -1,18 +1,33 @@
 import axios from '../config/axiosConfig';
 
-export const getPublicWords = async (vocab_id) => {
+export const getPublicWords = async (vocab_id, page) => {
     try {
+        console.log(vocab_id, page);
         const response = await axios.get(
-            `/api/vocabularies/public/${vocab_id}/words/`
+            `/api/vocabularies/public/${vocab_id}/words/range?page=${page}`
         );
-        // console.log(response.data.words);
+        console.log(response.data);
         if (!response.data.isAuthenticated) return null;
+        console.log(response.data.words);
         return response.data.words;
     } catch (error) {
         console.error('Error fetching vocabularies data:', error);
         throw error;
     }
 };
+// export const getPublicWords = async (vocab_id, page) => {
+//     try {
+//         const response = await axios.get(
+//             `/api/vocabularies/public/${vocab_id}/words/`
+//         );
+//         // console.log(response.data.words);
+//         if (!response.data.isAuthenticated) return null;
+//         return response.data.words;
+//     } catch (error) {
+//         console.error('Error fetching vocabularies data:', error);
+//         throw error;
+//     }
+// };
 
 export const addWord = async (vocab_id, inputValues) => {
     try {
@@ -20,7 +35,6 @@ export const addWord = async (vocab_id, inputValues) => {
             `/api/vocabularies/public/${vocab_id}/words/`,
             inputValues
         );
-        console.log(response);
         return { data: response.data, status: response.status };
     } catch (error) {
         var response = error.response;
